8fff42b9d83d8878c830f912c7b89c5b1df13ceb
kyle roux <kyle@level2designs.com>
2015-05-06 00:50:11 -0700

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='paver-pipeline',
     packages=['paver_pipeline'],
-    version='0.0.1',
+    version='0.0.2',
     author='kyle roux',
     author_email='kyle@level2designs.com',
     install_requires=[
